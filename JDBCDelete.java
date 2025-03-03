6e2018866e764b781581333f2f6739c8d0f92954
shweta kore <koreshweta03@gmail.com>
2025-03-03 18:05:30 +0530

    Add files via upload

---
+++ JDBCDelete.java
@@ -0,0 +1,32 @@
+package ANP_D0453;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class JDBCDelete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			//1.Register the Driver class
+			Class.forName("com.mysql.cj.jdbc.Driver");//oracle.jdbc.driver.OracleDriver
+			//2.Create connection
+			Connection con=null;
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/anp_d0453","root","security");
+			System.out.println(con);
+			//3.create statment
+			PreparedStatement ps=con.prepareStatement("delete from person where  person_id=?");
+			ps.setInt(1,6);
+			//4.Execute queries
+			int i=ps.executeUpdate();
+			System.out.println(i+"record deleted  sucessfully");
+			//5.Close connection
+			con.close();
+		}catch(Exception e){
+		System.out.println(e);
+		}
+
+	}
+
+}
